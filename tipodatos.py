91fb9909816477e3ee978c1806cb67fa0a1e681a
yairjr-code <eduardojad1102@gmail.com>
2025-02-22 09:45:36 -0500

    tipo de datos que se utilizan en cualquier leguaje de programacion

---
+++ tipodatos.py
@@ -0,0 +1,29 @@
+#Tipos de datos
+
+## Entero = int = integer es un numero entero
+
+año = 2023
+edad = 35
+
+#float  = float es un numero decimal
+
+pi = 3.13159
+medida = 1.35
+
+#string = str = cadena se utiliza para almacenar texto,
+#       las cadena se encierran en comillas dole ""  o  simples ''
+
+mensaje = "buenas noches"
+usuario = '@yairjr-code'
+
+## booleano = bool amacena dos true o false.
+
+falso = False
+verdadero = True
+
+
+##exiten mucho mas pero serian los primero que utlizaremos 
+
+
+
+
